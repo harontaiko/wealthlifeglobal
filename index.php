15eb578d71af1b2453a4e075ac42c6c296333507
harontaiko <harontaiko@gmail.com>
2021-04-07 21:16:30 +0300

    first commmit to wealth crypto

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="btc.png" type="image/x-icon">
+    <title>Company Title</title>
+</head>
+
+<body>
+    <section class="landing">
+        <div class="landing-inner">
+            <img src="./btc.png" />
+            <p>Trade, mine, and grow your crypto finance</p>
+            <h1>Coming Soon <span class="sm_title">in</span></h1>
+            <div class="countdown"></div>
+        </div>
+    </section>
+    <script src="landing.js"></script>
+</body>
+
+</html>

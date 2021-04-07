632f4c681f7e8db314888e0eed4a87fee6441a8a
harontaiko <harontaiko@gmail.com>
2021-04-07 21:17:14 +0300

    coming soon page

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <link rel="shortcut icon" href="btc.png" type="image/x-icon">
-    <title>Company Title</title>
+    <title>Wealth Life Global</title>
 </head>
 
 <body>
